7c43993171b5d4c66d097fc8b50a0a3ca0cb0ada
Denis <bidjada1102@gmail.com>
2023-04-06 14:47:46 +0200

    Form create Artist + Form update Artist +Form Delete Artist

---
+++ src/main/java/be/iccbxl/pid/reservationSpringBoot/model/Role.java
@@ -0,0 +1,71 @@
+package be.iccbxl.pid.reservationSpringBoot.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String role;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users = new ArrayList<>();
+
+
+    protected Role() {	}
+
+    public Role(String role) {
+        super();
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Role [id=" + id + ", role=" + role + "]";
+    }
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public Role addUser(User user) {
+        if(!this.users.contains(user)) {
+            this.users.add(user);
+            user.addRole(this);
+        }
+
+        return this;
+    }
+
+    public Role removeUser(User user) {
+        if(this.users.contains(user)) {
+            this.users.remove(user);
+            user.getRoles().remove(this);
+        }
+
+        return this;
+    }
+
+
+}
+
